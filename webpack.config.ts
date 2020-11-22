7a79a2a34dc6860fb3d1568805c1d4771db70f05
kkazum <49956319+kkazum@users.noreply.github.com>
2020-11-22 23:27:05 +0900

    deploy

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,4 +1,7 @@
 const path = require('path')
+import CopyWebpackPlugin from 'copy-webpack-plugin'
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -16,10 +19,21 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-    publicPath: 'dist/',
+    filename: 'popup.js',
+    publicPath: '/',
   },
+  plugins: [
+    new CopyWebpackPlugin({patterns: [
+      { from: 'src/public', to: '.'}
+    ]}),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'popup.html',
+    }),
+    new CleanWebpackPlugin()
+  ],
   devServer: {
+    contentBase: path.join(__dirname, 'src'),
     hot: true,
     open: true,
   }
